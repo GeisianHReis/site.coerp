fc37667c5caf7c506dc0ce8c220742c3a1c8b9e4
Geisian Reis <geisian.reis@levva.io>
2023-07-14 19:46:50 -0300

    qqqqqqq

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import logo from "../../assets/images.png";
+import qrCode from "../../assets/qrCode.jpeg";
 import Alert from "@mui/material/Alert";
 import { FooterStyle, Secao } from './styles';
 import { Snackbar } from '@mui/material';
@@ -34,7 +34,7 @@ export function Footer() {
             <Secao><WhatsappLogo size={28} color="white" /> (11) 9999-9999 </Secao>
             <Secao><MapPin size={28} /> <p>R: Quatorze de Outubro 235 </p> <p>Sítio Paredão - Ferraz de Vasconcelos</p> </Secao>
             <Secao><MapPin size={28} /><p> Rua João Cabral Melo Neto, 10</p><p> Cid.Tiradentes - SP </p></Secao>
-            <Secao><p>Contribua:</p> <p><img src={logo} /> </p> Chave PIX: <p id="texto">999.999.999.999 <Copy cursor={"pointer"} size={20} onClick={handleClick} /></p>
+            <Secao><p>Contribua:</p> <p><img src={qrCode} /> </p> Chave PIX: <p id="texto">999.999.999.999 <Copy cursor={"pointer"} size={20} onClick={handleClick} /></p>
 
             </Secao>
             <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
